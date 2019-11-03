754a9dcb10a5cc95ef6ffbe6c3ad56b6cf2358c5
tilapia39 <54948101+tilapia39@users.noreply.github.com>
2019-11-03 18:45:58 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,32 +1,26 @@
+<?php
+include('process.php'); // Includes Login Script
+if(isset($_SESSION['login_user'])){
+header("location: profile.php"); // Redirecting To Profile Page
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Page</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-
+<title>Login Form</title>
+<link href="style.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-	<div id="frm">
-		<form action="process.php" method="POST">
-		<p>
-			<label>Username:</label>
-			<input type="text" id="user" name="user"  />
-		</p>
-		
-		<p>
-			<label>Password:</label>
-			<input type="password" id="pass" name="pass"  />
-		</p>
-		
-		<p>
-			<input type="submit" id="btn" value="Login"  />
-		</p>
-	
-	</form>
-	</div>
-
+<div id="login">
+<h2>Login Form</h2>
+<form action="" method="post">
+<label>UserName :</label>
+<input id="name" name="username" placeholder="username" type="text">
+<label>Password :</label>
+<input id="password" name="password" placeholder="**********" type="password"><br><br>
+<input name="submit" type="submit" value=" Login ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
 </body>
-
 </html>
-
