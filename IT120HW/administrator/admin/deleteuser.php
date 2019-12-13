3e72c902d7a178538aeb914e2e3661b0248817d5
tilapia39 <54948101+tilapia39@users.noreply.github.com>
2019-12-13 09:09:42 -0500

    Update deleteuser.php

--- IT120HW/administrator/admin/deleteuser.php
+++ IT120HW/administrator/admin/deleteuser.php
@@ -1,11 +1,11 @@
  
 <?php 
  
-$user = "lat33";  
-$password = "j5L3ngLv";  
-$host = "sql1.njit.edu";  
-$dbase = "lat33";  
-$table = "login";  
+$user = "";  
+$password = "";  
+$host = "";  
+$dbase = "";  
+$table = "";  
  
 $username_entered= $_POST['usernameentered'];
 $email_entered= $_POST['emailentered']; 
